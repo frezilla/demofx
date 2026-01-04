519de1508e08ef6b6e2952b45f54d761acc97a78
fr3zill@ <fb.info@mailo.com>
2026-01-04 22:50:15 +0100

    Création de l'énumération Sizes

---
+++ src/main/java/eu/frezilla/demofx/tablet/configuration/Sizes.java
@@ -0,0 +1,18 @@
+package eu.frezilla.demofx.tablet.configuration;
+
+public enum Sizes {
+    LOW(new Size(320, 200)),
+    MEDIUM(new Size(640, 480)),
+    HIGH(new Size(800, 600));
+    
+    private final Size size;
+    
+    private Sizes(Size size) {
+        this.size = size;
+    }
+    
+    public Size getSize() {
+        return size;
+    }
+    
+}
