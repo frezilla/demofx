93654aad07a7b3575ff93e046d10774b83c98e6b
fr3zill@ <fb.info@mailo.com>
2026-01-05 17:23:38 +0100

    Création de la classe Tablet avec quelques méthodes rudimentaires

--- src/main/java/eu/frezilla/demofx/tablet/Tablet.java
+++ src/main/java/eu/frezilla/demofx/tablet/Tablet.java
@@ -2,13 +2,24 @@ package eu.frezilla.demofx.tablet;
 
 import eu.frezilla.demofx.tablet.configuration.Configuration;
 import java.util.Objects;
+import javax.swing.JFrame;
+import javax.swing.WindowConstants;
 
-public abstract class Tablet {
+public final class Tablet {
 
     private final Configuration configuration;
+    private final Display display;
+    private final JFrame frame;
 
-    Tablet(Configuration configuration) {
+    public Tablet(Configuration configuration) {
         this.configuration = Objects.requireNonNull(configuration, "La configuration n'est pas correctement définie");
+        this.display = new Display();
+        this.frame = new JFrame(this.configuration.getTitle());
+        
+        this.display.setPreferredSize(this.configuration.getSize().toDimension());
+        this.frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        this.frame.setResizable(false);
+        this.frame.add(display);
     }
 
     /**
@@ -17,8 +28,23 @@ public abstract class Tablet {
      * @return {@code Configuration}
      * @see Configuration
      */
-    public final Configuration getConfiguration() {
+    public Configuration getConfiguration() {
         return configuration;
     }
 
+    /**
+     * Affiche ou masque la tablette en fonction de la valeur du booléen
+     * ({@code True} : affichage; {@code False} : masquage)
+     *
+     * @param b booléen
+     */
+    public void setVisible(boolean b) {
+        if (b) {
+            frame.pack();
+            frame.setLocationRelativeTo(null);
+            frame.setVisible(true);
+        } else {
+            frame.setVisible(false);
+        }
+    }
 }
