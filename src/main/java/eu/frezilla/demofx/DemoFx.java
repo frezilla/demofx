4de17a0ebdd57172b9aa7274279945aa706ecc23
fr3zill@ <fb.info@mailo.com>
2026-01-09 22:47:42 +0100

    Test basique d'affichage à l'écran

--- src/main/java/eu/frezilla/demofx/DemoFx.java
+++ src/main/java/eu/frezilla/demofx/DemoFx.java
@@ -1,9 +1,18 @@
 package eu.frezilla.demofx;
 
+import eu.frezilla.demofx.drawing.Plot;
+import eu.frezilla.demofx.tablet.Tablet;
+import eu.frezilla.demofx.tablet.configuration.Configuration;
+
 public class DemoFx {
     
     public static void main(String[] args) {
         System.out.println("Hello World");
+        Configuration configuration = Configuration.getBuilder().build();
+        Tablet tablet = new Tablet(configuration);
+        tablet.add(new Plot());
+        tablet.setVisible(true);
+        tablet.display();
     }
     
 }
