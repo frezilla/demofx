e25f82d4bb08010cadd8f5ed730413328e817648
fr3zill@ <fb.info@mailo.com>
2026-01-10 22:19:47 +0100

    Enrichissement de la classe Plot

--- src/main/java/eu/frezilla/demofx/drawing/Plot.java
+++ src/main/java/eu/frezilla/demofx/drawing/Plot.java
@@ -1,16 +1,43 @@
 package eu.frezilla.demofx.drawing;
 
 import eu.frezilla.demofx.tablet.GContext;
+import java.awt.Color;
+import java.awt.Graphics2D;
 
 public class Plot extends Drawing {
     
-    public Plot() {
-        super(0);
+    private final int x;
+    private final int y;
+    
+    public Plot(int x, int y) {
+        this(x, y, Color.BLACK, 0);
+    }
+    
+    public Plot(int x, int y, Color color) {
+        this(x, y, color, 0);
+    }
+    
+    public Plot(int x, int y, Color color, int zIndex) {
+        super(color, zIndex);
+        this.x = x;
+        this.y = y;
     }
 
     @Override
     public void draw(GContext gContext) {
-        gContext.getG2d().drawLine(10, 10, 20, 20);
+        Graphics2D g = gContext.getG2d();
+        if ((x <= 0 || x >= gContext.getWidth())) return;
+        if ((y <= 0 || y >= gContext.getHeight())) return;
+        g.setColor(getColor());
+        g.drawLine(x, y, x, y);
+    }
+    
+    public int getX() {
+        return x;
+    }
+    
+    public int getY() {
+        return y;
     }
     
 }
