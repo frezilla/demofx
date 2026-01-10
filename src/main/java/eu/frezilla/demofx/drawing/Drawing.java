e051105f84bf1f57bfd9cab4cd9778a689f083a6
fr3zill@ <fb.info@mailo.com>
2026-01-10 22:11:21 +0100

    Ajout de la couleur

--- src/main/java/eu/frezilla/demofx/drawing/Drawing.java
+++ src/main/java/eu/frezilla/demofx/drawing/Drawing.java
@@ -1,16 +1,24 @@
 package eu.frezilla.demofx.drawing;
 
 import eu.frezilla.demofx.tablet.GContext;
+import java.awt.Color;
+import java.util.Objects;
 
 public abstract class Drawing  {
     
+    private final Color color;
     private final int zIndex;
         
-    protected Drawing(int zIndex) {
+    protected Drawing(Color color, int zIndex) {
+        this.color = Objects.requireNonNull(color);
         this.zIndex = zIndex;
     }
     
     public abstract void draw(GContext gContext);
+    
+    public final Color getColor() {
+        return color;
+    }
     
     public final int getZIndex() {
         return zIndex;
