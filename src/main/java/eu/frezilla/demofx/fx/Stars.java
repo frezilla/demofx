597eff03b948def0f4d3499eb6c5b661754f6a60
fr3zill@ <34129644+frezilla@users.noreply.github.com>
2026-01-16 17:30:52 +0100

    Affichage d'un polygone (un triangle) à l'écran

--- src/main/java/eu/frezilla/demofx/fx/Stars.java
+++ src/main/java/eu/frezilla/demofx/fx/Stars.java
@@ -2,6 +2,7 @@ package eu.frezilla.demofx.fx;
 
 import eu.frezilla.demofx.drawing.Line;
 import eu.frezilla.demofx.drawing.Plot;
+import eu.frezilla.demofx.drawing.Triangle;
 import eu.frezilla.demofx.fx.configuration.Configuration;
 import java.awt.Color;
 
@@ -20,6 +21,7 @@ public class Stars extends Fx {
         y++;
         getTablet().add(new Line(x - 10, y + 10, x + 10, y - 10, Color.RED));
         getTablet().add(new Line(x - 10, y - 10, x + 10, y + 10, Color.GREEN));
+        getTablet().add(new Triangle(x + 20, y, x+40, y, x+30, y+20, Color.BLUE));
         getTablet().add(new Plot(x, y, Color.CYAN));
     } 
     
