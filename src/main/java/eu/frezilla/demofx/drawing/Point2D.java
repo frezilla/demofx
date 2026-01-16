398f6a78beff63c0908fb223af02703e5592b42d
fr3zill@ <fb.info@mailo.com>
2026-01-16 17:14:46 +0100

    Création de la classe Point2D

---
+++ src/main/java/eu/frezilla/demofx/drawing/Point2D.java
@@ -0,0 +1,20 @@
+package eu.frezilla.demofx.drawing;
+
+public final class Point2D {
+    
+    private final int x;
+    private final int y;
+    
+    public Point2D(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+    
+    public int getX() {
+        return x;
+    }
+    
+    public int getY() {
+        return y;
+    }
+}
