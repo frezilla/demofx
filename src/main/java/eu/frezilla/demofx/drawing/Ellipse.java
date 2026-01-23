d441ed9003bb5afa9cbceaa669e0c57eb3d5316e
fr3zill@ <34129644+frezilla@users.noreply.github.com>
2026-01-23 16:03:36 +0100

    Définition de la classe comme finale

--- src/main/java/eu/frezilla/demofx/drawing/Ellipse.java
+++ src/main/java/eu/frezilla/demofx/drawing/Ellipse.java
@@ -5,7 +5,7 @@ import java.awt.Color;
 import java.util.ArrayList;
 import java.util.List;
 
-public class Ellipse implements Drawing {
+public final class Ellipse implements Drawing {
     
     private final Polygon polygon;
     
